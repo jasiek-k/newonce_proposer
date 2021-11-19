3664947b0fc18c9b5502dc65da288d8c70d4307f
jasiek-k <janek9805@gmail.com>
2021-11-19 19:27:30 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,53 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import { initializeApp } from "firebase/app";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Example from "./Example/Example.component";
-import FirebaseExample from "./FirebaseExample/FirebaseExample";
+// import Example from "./Example/Example.component";
+// import FirebaseExample from "./FirebaseExample/FirebaseExample";
 
 import firebaseConfig from "./config/firebase";
+import {
+  GENERATOR_ROUTE,
+  HOME_ROUTE,
+  PLAYLIST_ROUTE,
+  PROFILE_ROUTE,
+} from "./config/config";
+import Home from "./modules/home/Home.page";
+import Profile from "./modules/profile/Profile.page";
+import Playlist from "./modules/playlist/Playlist.page";
+import Generator from "./modules/generator/Generator.page";
 
-function App() {
+const App = () => {
   const queryClient = new QueryClient();
   const app = initializeApp(firebaseConfig);
 
+  console.log(app);
+
   return (
-    <div>
-      <header></header>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <Example />
-          <FirebaseExample />
-        </RecoilRoot>
-      </QueryClientProvider>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        {/* <Example />
+        <FirebaseExample /> */}
+        <Router>
+          <Switch>
+            <Route path={HOME_ROUTE} exact={true}>
+              <Home />
+            </Route>
+            <Route path={PROFILE_ROUTE}>
+              <Profile />
+            </Route>
+            <Route path={PLAYLIST_ROUTE}>
+              <Playlist />
+            </Route>
+            <Route path={GENERATOR_ROUTE}>
+              <Generator />
+            </Route>
+          </Switch>
+        </Router>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
---
+++ src/modules/generator/Generator.page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Generator: React.FC = () => <div>Generator</div>;
+
+export default Generator;
