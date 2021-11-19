7260bc91bf65e621b12214b00c9ae8e28cee41ea
Kacper Cieciński <ciecinski.kacper@gmail.com>
2021-11-19 23:44:06 +0100

    basic form adde with formik

--- src/modules/generator/Generator.page.tsx
+++ src/modules/generator/Generator.page.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { useQuery } from "react-query";
 import Header from "../commons/Header.component";
+import { fetchMostRatedAlbums } from "./Generator.service";
+import GeneratorForm from "./GeneratorForm.component";
 
-const Generator: React.FC = () => (
-  <div>
-    <Header />
-  </div>
-);
+const Generator: React.FC = () => {
+
+  const { data, error, isError, isLoading } = useQuery("albums", fetchMostRatedAlbums);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error! {(error as any)?.message}</div>;
+  }
+
+  const generatePlaylist = (data: {
+    name: string;
+    password: string;
+    albumsIds: string[];
+  }) =>  {};
+
+  return (
+    <div>
+      <Header />
+      <GeneratorForm albums={data?.data|| []} onSubmit={generatePlaylist} />
+    </div>
+  );
+};
 
 export default Generator;
