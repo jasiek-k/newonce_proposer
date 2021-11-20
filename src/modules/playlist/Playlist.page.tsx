56e98551dba6729b736ca52376291953a7d31397
Kacper Cieciński <ciecinski.kacper@gmail.com>
2021-11-20 10:56:58 +0100

    confrim or remove new song

--- src/modules/playlist/Playlist.page.tsx
+++ src/modules/playlist/Playlist.page.tsx
@@ -1,49 +1,109 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
+import SpotifyApiService from "../../SpotifyIntegration/SpotifyApi.service";
 import useCreateSpotifyPlaylist from "../../SpotifyIntegration/useCreateSpotifyPlaylist.hook";
 import Header from "../commons/Header.component";
 import { TracksToPlaylistDTO } from "../generator/Generator.types";
 import AddToPlaylist from "./AddToPlaylist.component";
 import { playlistState } from "./Playlist.state";
 
-export const formatSearchString = (track: TracksToPlaylistDTO) => `${track.name}, ${track.album}, ${track.artist}`;
+export const formatSearchString = (track: TracksToPlaylistDTO) =>
+  `${track.name}, ${track.album}, ${track.artist}`;
 
 const Playlist: React.FC = () => {
   // TODO: fetching from database
-  const currentPlaylist = useRecoilValue(playlistState);
+  const [currentPlaylist, setCurrentPlaylist] = useRecoilState(playlistState);
 
   // TODO: validate password, or mayby if limited time let's not implement that feature?
 
-  const Player = useCreateSpotifyPlaylist(currentPlaylist?.tracks.map((track) => formatSearchString(track)), currentPlaylist?.name)
-  
+  const Player = useCreateSpotifyPlaylist(
+    currentPlaylist?.tracks.map((track) => formatSearchString(track)),
+    currentPlaylist?.name
+  );
 
-  if(!currentPlaylist) {
+  const activateTrack = async (track: TracksToPlaylistDTO) => {
+    // TODO call to api
+    setCurrentPlaylist((curr) =>
+      curr
+        ? {
+            ...curr,
+            tracks: curr.tracks.map((ltrack) =>
+              ltrack.name === track.name
+                ? {
+                    ...ltrack,
+                    isActive: true,
+                  }
+                : ltrack
+            ),
+          }
+        : curr
+    );
+
+    const trackUri = (
+      await SpotifyApiService.getTrack(formatSearchString(track))
+    ).tracks.items[0].uri;
+    console.log(trackUri);
+    const playlists = await SpotifyApiService.getPlaylists();
+    const playlist = playlists?.items.find(
+      (playlist) => playlist.name === currentPlaylist?.name
+    );
+
+    if (playlist) {
+      await SpotifyApiService.addToTrackToPlaylis(playlist.id, [trackUri]);
+    }
+  };
+
+  const removeTrack = (track: TracksToPlaylistDTO) => {
+    setCurrentPlaylist((curr) =>
+      curr
+        ? {
+            ...curr,
+            tracks: curr.tracks.filter((ltrack) => ltrack.name !== track.name),
+          }
+        : curr
+    );
+  };
+
+  if (!currentPlaylist) {
     return (
       <div>
         <Header />
         loading
       </div>
-    )
+    );
   }
 
-  return(
-      <div>
+  return (
+    <div>
       <Header />
+      <div>{currentPlaylist.name}</div>
       <div>
-        {currentPlaylist.name}
-      </div>
-      <div>
-      TWOJE UTWORY
-      <ul>
-        {currentPlaylist.tracks.map((track) => (
-          <li>
-            <div>
-            <img className="object-contain h-48" src={track.imgSrc} alt="trackImage" />
-            <span>{track.name} - {track.artist} - {track.duration}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+        TWOJE UTWORY
+        <ul>
+          {currentPlaylist.tracks.map((track) => (
+            <li>
+              <div className={track.isActive ? "bg-green-400" : "bg-gray-400"}>
+                {!track.isActive && (
+                  <>
+                    <button onClick={() => activateTrack(track)}>
+                      Add to playlist
+                    </button>
+                    <button onClick={() => removeTrack(track)}>Remove</button>
+                  </>
+                )}
+
+                <img
+                  className="object-contain h-48"
+                  src={track.imgSrc}
+                  alt="trackImage"
+                />
+                <span>
+                  {track.name} - {track.artist} - {track.duration}
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
       <div>
         <AddToPlaylist />
@@ -51,7 +111,6 @@ const Playlist: React.FC = () => {
       {Player && <Player />}
     </div>
   );
-}
-
+};
 
 export default Playlist;
